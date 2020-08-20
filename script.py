29748245677717ddab1e761c3f3c12910197bccb
Nick White <git@njw.name>
2020-08-20 09:51:30 +0100

    Use a lockdir to avoid sending commands when one is already being processed
    The lockdir is removed if the addon is called a second time, to avoid the
    case of a stale lockdir preventing the addon from functioning.

--- script.py
+++ script.py
@@ -3,9 +3,30 @@ import re
 import subprocess
 import xbmc
 import xbmcgui
+import xbmcplugin
+import xbmcvfs
+
+lockdir = 'special://temp/kodi.script.togglepower.inprogress/'
+
+exists = xbmcvfs.exists(lockdir)
+if exists:
+	xbmc.log('Kodi.Script.ToggleLGTVPower: Not toggling power; lockfile exists', xbmc.LOGNOTICE)
+	# we'll remove the lockdir here, just to avoid the whole functionality seizing up
+	# if the dir is created and not destroyed for some reason. it's unlikely that a user
+	# will try to power off more than once after initiating power off, and it's better
+	# to occasionally fail to detect this than to effectively disable the plugin as
+	# something went awry.
+	success = xbmcvfs.rmdir(lockdir)
+	if not success:
+		xbmc.log('Kodi.Script.ToggleLGTVPower: Failed to remove lock directory', xbmc.LOGERROR)
+	quit()
+
+success = xbmcvfs.mkdir(lockdir)
+if not success:
+	xbmc.log('Kodi.Script.ToggleLGTVPower: Failed to create lock directory', xbmc.LOGERROR)
 
 d = xbmcgui.DialogProgressBG()
-d.create('POWER', 'Just a moment while I check whether I need to turn the TV on or off')
+d.create('Power', 'Just a moment while I check whether I need to turn the TV on or off')
 
 xbmc.log("Toggling TV power", xbmc.LOGNOTICE)
 
@@ -17,6 +38,9 @@ except:
 	xbmc.log("Kodi.Script.ToggleLGTVPower: cec-client command to get TV status failed, may wake TV anyway", xbmc.LOGERROR)
 	d.update(100, 'Failed to get TV status, may wake TV anyway')
 	d.close()
+	success = xbmcvfs.rmdir(lockdir)
+	if not success:
+		xbmc.log('Kodi.Script.ToggleLGTVPower: Failed to remove lock directory', xbmc.LOGERROR)
 	quit()
 reStatus = re.compile(b'power status: (.+)')
 match = re.search(reStatus, s)
@@ -29,7 +53,7 @@ else:
 if state == 'on':
 	d.update(50, 'Turning off TV')
 	xbmc.log("Kodi.Script.ToggleLGTVPower: Turning off TV with Kodi.Script.TurnOffLGTV", xbmc.LOGNOTICE)
-	xbmc.executebuiltin('RunAddon(kodi.script.turnofflgtv)')
+	xbmc.executebuiltin('RunAddon(kodi.script.turnofflgtv)', True)
 else:
 	d.update(50, 'Turning on TV')
 	xbmc.log("Kodi.Script.ToggleLGTVPower: Turning on TV with cec-client", xbmc.LOGNOTICE)
@@ -43,3 +67,7 @@ else:
 
 d.update(100, 'Done')
 d.close()
+
+success = xbmcvfs.rmdir(lockdir)
+if not success:
+	xbmc.log('Kodi.Script.ToggleLGTVPower: Failed to remove lock directory', xbmc.LOGERROR)
