9bd36d6c6804842ea2ec3036e0edae7b4f4a50cc
Nick White <git@njw.name>
2020-08-20 08:44:11 +0100

    Add DialogProgressBG() messages on status of power commands
    As it takes around 1 second for the CEC status command to complete, and
    then a little longer for the actual turn off TV command to execute, the
    power button feels very sluggish, and users can assume that their
    keypress may not have been registered. A dialogue message to communicate
    that something is happening helps remedy this.

--- script.py
+++ script.py
@@ -2,6 +2,10 @@
 import re
 import subprocess
 import xbmc
+import xbmcgui
+
+d = xbmcgui.DialogProgressBG()
+d.create('POWER', 'Just a moment while I check whether I need to turn the TV on or off')
 
 xbmc.log("Toggling TV power", xbmc.LOGNOTICE)
 
@@ -11,6 +15,8 @@ try:
 	s = subprocess.check_output('echo pow 0 | cec-client -s -d 1 -m', shell=True)
 except:
 	xbmc.log("Kodi.Script.TogglePower: cec-client command to get TV status failed, may wake TV anyway", xbmc.LOGERROR)
+	d.update(100, 'Failed to get TV status, may wake TV anyway')
+	d.close()
 	quit()
 reStatus = re.compile(b'power status: (.+)')
 match = re.search(reStatus, s)
@@ -21,9 +27,11 @@ else:
 	state = 'unknown'
 
 if state == 'on':
+	d.update(50, 'Turning off TV')
 	xbmc.log("Kodi.Script.TogglePower: Turning off TV with Kodi.Script.TurnOffLGTV", xbmc.LOGNOTICE)
 	xbmc.executebuiltin('RunAddon(kodi.script.turnofflgtv)')
 else:
+	d.update(50, 'Turning on TV')
 	xbmc.log("Kodi.Script.TogglePower: Turning on TV with cec-client", xbmc.LOGNOTICE)
 	# Running cec-client has a habit of disabling Kodi's CEC connection,
 	# so we can't use its inbuilt CECActivateSource command
@@ -32,3 +40,6 @@ else:
 		subprocess.call('echo on 0 | cec-client -s -d 1 -m', shell=True)
 	except:
 		xbmc.log("Kodi.Script.TogglePower: cec-client command to turn on TV failed", xbmc.LOGERROR)
+
+d.update(100, 'Done')
+d.close()
